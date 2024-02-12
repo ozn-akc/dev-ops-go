fc3717d444608a7706d9b1384ed43c88fddeb708
Ozan Akcebe <ozan.akcebe@breuninger.de>
2024-02-12 18:32:57 +0100

    refactor: change product data in internal.go

--- internal/internal.go
+++ internal/internal.go
@@ -18,9 +18,9 @@ type Product struct {
 
 // Static data for three products
 var products = []Product{
-	{ID: 1, Name: "Product 1", Price: 19.99},
-	{ID: 2, Name: "Product 2", Price: 29.99},
-	{ID: 3, Name: "Product 3", Price: 39.99},
+	{ID: 1, Name: "T-Shirt", Price: 19.99},
+	{ID: 2, Name: "Hose", Price: 29.99},
+	{ID: 3, Name: "Raspberry Pi 4", Price: 39.99},
 }
 
 func AuthLoginHandler(w http.ResponseWriter, r *http.Request) {
